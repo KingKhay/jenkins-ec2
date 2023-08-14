f5ef1af752e34a4594b9610872fd0e1b37e96967
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-08-14 11:50:53 +0000

    added test controller

---
+++ src/main/java/com/deployment/jenkinsec2/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.deployment.jenkinsec2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    String test(){
+        return "Hello World.";
+    }
+}
